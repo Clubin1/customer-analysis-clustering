59b646de66442b3a8559a1b38303f288e7f9ad26
Justin Tubay <jtubay@uncc.edu>
2024-04-15 00:58:33 -0400

    fix bugs and add pairplot

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import matplotlib.pyplot as plt
-
-# function to find wcss
-def find_wcss():
-    wcss = []
-    for i in range(1, 11):
-        kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
-        kmeans.fit(data_scaled)
-        wcss.append(kmeans.inertia_)
+import seaborn as sns
 
 # get datafram
-df = pd.read_csv('marketing_campaign.csv', sep='\t')
+data = pd.read_csv('marketing_campaign.csv', sep='\t')
+
+# pairplot for analysis, takes awhile to run so commenting out
+# sns.pairplot(df)
+# plt.show()
 
 # preprocessing
-data = df.dropna()
+data = data.dropna()
 data['Dt_Customer'] = pd.to_datetime(data['Dt_Customer'], format='%d-%m-%Y')
 categorical_columns = ['Education', 'Marital_Status']
 encoder = LabelEncoder()
@@ -29,7 +26,11 @@ scaler = StandardScaler()
 data_scaled = scaler.fit_transform(data[numerics])
 
 # find wcss
-wcss = find_wcss()
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(data_scaled)
+    wcss.append(kmeans.inertia_)
 
 # plot for elbow method
 plt.plot(range(1, 11), wcss)
@@ -39,7 +40,7 @@ plt.title('Elbow Method')
 plt.show()
 
 # use the most optimal k to form k clusters and print summary
-k = 4  
+k = 4
 km = KMeans(n_clusters=k, init='k-means++', random_state=42)
 data['Cluster'] = km.fit_predict(data_scaled)
 summary = data.groupby('Cluster').mean()
